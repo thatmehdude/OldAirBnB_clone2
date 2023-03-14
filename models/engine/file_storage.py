46dc12abd71e6654d99d88f542afff8aab0fd29c
thatmehdude <104757542+thatmehdude@users.noreply.github.com>
2023-03-14 14:32:06 +0100

    made changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,8 +17,9 @@ def new(self, obj):
     """sets in __object the obj with key <obj class name>.id"""
     obj_class_name = obj.__class__.__name__
     obj_id = obj.id
-    key = obj_class_name + "." + obj_id
-    FileStorage.__objects[key] = obj
+    key_name = obj_class_name + "." + obj_id
+
+    FileStorage.__objects[key_name] = obj
 
 def save(self):
     """seralizes __objects to JSON file"""
@@ -30,18 +31,13 @@ def save(self):
 
 def reload(self):
     """
-    dserializes the JSON file 
-    does nothing if file is not found
+     deserializes the JSON file to __objects
+     does nothing if file is not found
     """
-    def reload(self):
-        """
-        deserializes the JSON file to __objects
-        does nothing if file is not found
-        """
-        try:
-            with open(FileStorage.__file_path, "r") as read_file:
-                dummy_dict = json.load(read_file)
-                for k, v in dummy_dict.items():
-                    FileStorage.__objects[k] = eval(v['__class__'])(**v)
-        except FileNotFoundError:
+    try:
+        with open(FileStorage.__file_path, "r") as read_file:
+            dummy_dict = json.load(read_file)
+            for k, v in dummy_dict.items():
+                FileStorage.__objects[k] = eval(v['__class__'])(**v)
+    except FileNotFoundError:
             pass
